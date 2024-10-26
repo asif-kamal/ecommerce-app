844561753279b44bf50cce466fe813bf8e48b2bc
asif-kamal <asif.kamal1489@gmail.com>
2024-10-25 20:08:05 -0400

    testing app component with tailwindcss

--- ecommerce-app/src/App.tsx
+++ ecommerce-app/src/App.tsx
@@ -1,7 +1,11 @@
-import './App.css'
+import "./App.css";
 
 function App() {
-  
+  return (
+    <div>
+      <h2 className="text-3xl font-bold underline">Hello</h2>
+    </div>
+  );
 }
 
-export default App
+export default App;
