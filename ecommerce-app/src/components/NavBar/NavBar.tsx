411b0410dddc2fc38b488d5b1544238666488490
asif-kamal <asif.kamal1489@gmail.com>
2024-10-26 20:25:51 -0400

    added styling for NavBar links, logo, searchbar and icons

--- ecommerce-app/src/components/NavBar/NavBar.tsx
+++ ecommerce-app/src/components/NavBar/NavBar.tsx
@@ -1,24 +1,78 @@
 import React from "react";
+import WishList from "../Common/WishList";
+import AccountIcon from "../Common/AccountIcon";
+import CartIcon from "../Common/CartIcon";
 
 const Navigation: React.FC = () => {
   return (
-    <nav className="flex items-center py-6 px-16 justify-between gap-40">
+    <nav className="flex items-center py-6 px-16 justify-between">
       {/* Logo aligned to the top left */}
       <div>
-      <a className="text-3xl text-black gap-8" href="/">ecommercews</a>
+        <a className="text-3xl text-black" href="/">
+          ecommercews
+        </a>
       </div>
+
       {/* Nav items aligned to the top right and displayed right-to-left */}
-      <div className="flex flex-wrap items-center gap-10 flex-1">
-        <ul className="flex">
-          <a href="/" className="text-lg font-normal text-black">
-            Shop
-          </a>
-          <a href="/about" className="text-lg font-normal text-black">
-            About
-          </a>
-          <a href="/contact" className="text-lg font-normal text-black">
-            Contact
-          </a>
+      <div className="flex flex-1 justify-end items-center">
+        <ul className="flex flex-row-reverse gap-10 pr-8">
+          <li>
+            <a href="/computers" className="text-lg font-normal text-black">
+              Computers
+            </a>
+          </li>
+          <li>
+            <a href="/tv" className="text-lg font-normal text-black">
+              TV
+            </a>
+          </li>
+          <li>
+            <a href="/audiovideo" className="text-lg font-normal text-black">
+              Audio & Video
+            </a>
+          </li>
+        </ul>
+
+        {/* Search Bar aligned with links */}
+        <div className="flex justify-center ml-10 items-center">
+          <div className="border rounded flex overflow-hidden">
+            <div className="flex items-center justify-center px-6">
+              <svg
+                className="h-4 w-4 text-gray-600"
+                fill="currentColor"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+              >
+                <path d="M10 2a8 8 0 105.293 14.293l5.707 5.707 1.414-1.414-5.707-5.707A8 8 0 0010 2zm0 2a6 6 0 110 12 6 6 0 010-12z" />
+              </svg>
+              <input
+                type="text"
+                className="px-4 py-2 outline-none"
+                placeholder="Search..."
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* Additional Action Item Icons */}
+      <div className="flex flex-wrap items-center ml-6">
+        <ul className="flex items-center gap-8">
+          <li>
+            <button>
+              <WishList />
+            </button>
+          </li>
+          <li>
+            <button>
+              <AccountIcon />
+            </button>
+          </li>
+          <li>
+            <a href="/cart-items">
+              <CartIcon />
+            </a>
+          </li>
         </ul>
       </div>
     </nav>
