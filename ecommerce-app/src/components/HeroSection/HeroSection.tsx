b16fe0b3f277ede4a4e86e794b1652d9c33b941a
asif-kamal <asif.kamal1489@gmail.com>
2024-10-27 14:31:09 -0400

    added TailWind CSS for text inside banner

--- ecommerce-app/src/components/HeroSection/HeroSection.tsx
+++ ecommerce-app/src/components/HeroSection/HeroSection.tsx
@@ -3,9 +3,25 @@ import HeroImg from "../../assets/img/radek-grzybowski-unsplash.jpg";
 const HeroSection = () => {
   return (
     <div
-      className="relative flex items-center bg-cover text-start bg-center h-screen w-full"
+      className="relative flex items-center bg-cover bg-center text-left h-screen w-full"
       style={{ backgroundImage: `url(${HeroImg})` }}
-    ></div>
+    >
+      <div className="absolute top-0 right-0 bottom-0 left-0"></div>
+      <main className="px-10 lg:px-24 z-10">
+        <div className="text-left">
+          <h2 className="text-2xl text-white text-outline">Computers</h2>
+        </div>
+        <p className="mt-3 text-white text-outline sm:mt-5 sm:max-w-xl text-6xl">
+          Deals for the Holidays
+        </p>
+        <p className="mt-3 text-white text-outline sm:mt-5 sm:max-w-xl text-2xl">
+          Feature-Rich / Intuitive / Powerful
+        </p>
+        <button className="border rounded mt-6 border-gray-400 text-white bg-black w-44 h-12">
+          Shop Now
+        </button>
+      </main>
+    </div>
   );
 };
 
