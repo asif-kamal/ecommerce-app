b97088dcc0fb265ddb8cb5f2ea1d472127e0cdb7
asif-kamal <asif.kamal1489@gmail.com>
2024-10-27 21:26:15 -0400

    created a constants file for different responsiveness for mobile, tablet, and desktop carousel usage

---
+++ ecommerce-app/src/utils/Section.constants.ts
@@ -0,0 +1,19 @@
+import { ResponsiveType } from "react-multi-carousel";
+
+export const responsive: ResponsiveType = {
+  desktop: {
+    breakpoint: { max: 3000, min: 1024 },
+    items: 4,
+    slidesToSlide: 4,
+  },
+  tablet: {
+    breakpoint: { max: 1024, min: 768 },
+    items: 4,
+    slidesToSlide: 4,
+  },
+  mobile: {
+    breakpoint: { max: 767, min: 464 },
+    items: 2,
+    slidesToSlide: 1,
+  },
+};
