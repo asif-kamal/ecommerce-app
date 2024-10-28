1ea2cbe8e2ab445656b40748df2382acf8b38b14
asif-kamal <asif.kamal1489@gmail.com>
2024-10-28 19:13:38 -0400

    added description and actionArrow attributes to Card component

--- ecommerce-app/src/components/Card/Card.tsx
+++ ecommerce-app/src/components/Card/Card.tsx
@@ -1,9 +1,11 @@
 interface CardProps {
   imagePath: string;
   title: string;
+  description: string;
+  actionArrow: boolean
 }
 
-const Card = ({ imagePath, title }: CardProps) => {
+const Card = ({ imagePath, title, description, actionArrow }: CardProps) => {
   return (
     <div className="flex items-center flex-col p-8">
       <img
@@ -11,7 +13,13 @@ const Card = ({ imagePath, title }: CardProps) => {
         src={imagePath}
         alt="electronics"
       />
-      <p className="text-[16px] pt-[5px]">{title}</p>
+      <div className="flex justify-between">
+        <div className="flex flex-col p-5">
+          <p className="text-[16px] p-2">{title}</p>
+          {description && <p className="text-[14px]">{description}</p>}
+        </div>
+        {actionArrow && <div></div>}
+      </div>
     </div>
   );
 };
