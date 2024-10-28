57af3ca5d59222e7491c1789088c5c5630a23065
asif-kamal <asif.kamal1489@gmail.com>
2024-10-27 21:23:44 -0400

    created dummie data for cards in Carousel

---
+++ ecommerce-app/src/components/Section/NewProducts.tsx
@@ -0,0 +1,57 @@
+import SectionHeading from "./SectionHeading/SectionHeading";
+import Card from "../Card/Card";
+import favicon from "../../assets/icon/electronics_icon.png";
+import { responsive } from "../../utils/Section.constants";
+import Carousel from "react-multi-carousel";
+
+const items = [
+  {
+    id: 1,
+    title: "Item",
+    imagePath: favicon,
+  },
+  {
+    id: 2,
+    title: "Item",
+    imagePath: favicon,
+  },
+  {
+    id: 3,
+    title: "Item",
+    imagePath: favicon,
+  },
+  {
+    id: 4,
+    title: "Item",
+    imagePath: favicon,
+  },
+  {
+    id: 5,
+    title: "Item",
+    imagePath: favicon,
+  }
+];
+
+const NewProducts = () => {
+    return (
+      <>
+      <SectionHeading title={'New Arrivals'}/>
+      <Carousel
+          responsive={responsive}
+          autoPlay={false}
+          swipeable={true}
+          draggable={false}
+          showDots={false}
+          infinite={false}
+          partialVisible={false}
+          itemClass={'react-slider-custom-item'}
+          className='px-8'
+        >
+          {items && items?.map((item,index)=> <Card key={item?.title +index} title={item.title} imagePath={item.imagePath}/>)}
+  
+        </Carousel>
+      </>
+    )
+  }
+
+export default NewProducts;
