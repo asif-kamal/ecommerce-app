2ebd4d05f99082054f2115c4bba2555ada69ba88
asif-kamal <asif.kamal1489@gmail.com>
2024-10-28 21:38:10 -0400

    Create Category components only if content exists

--- ecommerce-app/src/App.tsx
+++ ecommerce-app/src/App.tsx
@@ -1,6 +1,8 @@
 import HeroSection from "./components/HeroSection/HeroSection";
 import Navigation from "./components/NavBar/NavBar";
+import Category from "./components/Section/Category/Category";
 import NewProducts from "./components/Section/NewProducts";
+import content from "./data/content.json";
 
 function App() {
   return (
@@ -8,6 +10,13 @@ function App() {
       <Navigation />
       <HeroSection />
       <NewProducts />
+      {content?.category &&
+        content?.category.map((item, index) => (
+          <Category
+            key={item?.title + index}
+            {...item}
+          />
+        ))}
     </>
   );
 }
