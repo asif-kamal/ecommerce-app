edee6938263b3bb2cdda9cb5398230e1f37cb79e
asif-kamal <asif.kamal1489@gmail.com>
2024-10-29 21:23:25 -0400

    changed the padding for the text to align underneath the image

--- ecommerce-app/src/components/Card/Card.tsx
+++ ecommerce-app/src/components/Card/Card.tsx
@@ -16,7 +16,7 @@ const Card = ({
   width,
 }: CardProps) => {
   return (
-    <div className="flex items-start flex-col p-8">
+    <div className="flex items-start flex-col pl-12">
       <img
         className={`h-[${height ? height : "240px"}] w-[${width ? width : "240px"}] bg-cover bg-center border rounded hover:scale-105 cursor-pointer`}
         src={imagePath}
@@ -24,8 +24,8 @@ const Card = ({
       />
       <div className="w-full">
         <div className="flex flex-col text-left p-5">
-          <p className="text-[16px] p-2">{title}</p>
-          {description && <p className="text-[14px] px-2 text-gray-500">{description}</p>}
+          <p className="text-[16px] p-0">{title}</p>
+          {description && <p className="text-[14px] p-0 text-gray-500">{description}</p>}
         </div>
         {actionArrow && <div className="text-left px-2">→</div>}
       </div>
