71311c83bb1ff2615f9c23127d5fc5f9d5ceb9b8
asif-kamal <asif.kamal1489@gmail.com>
2024-11-03 10:52:20 -0500

    added fbIcon and instaIcon components in Footer, along with styling

--- ecommerce-app/src/components/Footer/Footer.tsx
+++ ecommerce-app/src/components/Footer/Footer.tsx
@@ -1,3 +1,6 @@
+import FacebookIcon from "../Common/FacebookIcon";
+import InstaIcon from "../Common/InstagramIcon";
+
 interface FooterProps {
   content: {
     items: (
@@ -21,16 +24,21 @@ const Footer: React.FC<FooterProps> = ({ content }) => {
           content?.items?.map((item, index) => {
             return (
               <div className="flex flex-col">
-                <p>{item?.title}</p>
+                <p className="text-[16px] pb-[10px]">{item?.title}</p>
                 {item?.list &&
                   item?.list?.map((listItem, index) => (
-                    <a className="flex flex-col" href={listItem?.path}>
+                    <a className="flex flex-col text-[14px] py-2" href={listItem?.path}>
                       {listItem?.label}
                     </a>
                   ))}
+                  {item?.description && <p className="">{item?.description}</p>}
               </div>
             );
           })}
+      </div>
+      <div className="flex gap-2 items-center justify-center py-4">
+          <a href="/fb"><FacebookIcon /></a>
+          <a href="/ig"><InstaIcon /></a>
       </div>
       <p className="text-sm text-white text-center content-center">
         {content?.copyright}
