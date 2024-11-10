8fa5b1d117766740c0d2816cd5dc490b83bcc0dc
asif-kamal <asif.kamal1489@gmail.com>
2024-11-10 17:52:10 -0500

    added NavBar and Outlet to the component

--- ecommerce-app/src/pages/StoreAppWrapper.tsx
+++ ecommerce-app/src/pages/StoreAppWrapper.tsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import { Outlet } from 'react-router-dom'
+import NavBar from '../components/NavBar/NavBar'
 
 const StoreAppWrapper = () => {
   return (
-    <div>StoreAppWrapper</div>
+    <div>
+        <NavBar />
+        <Outlet />
+    </div>
   )
 }
 
