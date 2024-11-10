e0cb94ee26dfd4749b54f4ac134cdad832012b4f
asif-kamal <asif.kamal1489@gmail.com>
2024-11-10 17:54:38 -0500

    created new paths for PLP and App comps, inside of SAW for consistency throughout pages

--- ecommerce-app/src/routes.tsx
+++ ecommerce-app/src/routes.tsx
@@ -1,14 +1,21 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "./App";
+import StoreAppWrapper from "./pages/StoreAppWrapper";
 import ProductListPage from "./pages/ProductListPage/ProductListPage";
 
 export const router = createBrowserRouter([
   {
     path: "/",
-    element: <App />,
-  },
-  {
-    path: "/audiovideo",
-    element: <ProductListPage />,
+    element: <StoreAppWrapper />,
+    children: [
+      {
+        path:"/",
+        element: <App />
+      },
+      {
+        path:"/audiovideo",
+        element: <ProductListPage />
+      }
+    ]
   },
 ]);
