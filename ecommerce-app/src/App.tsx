3674f857e9e287ada4bac9c691d0c817f59afdbd
asif-kamal <asif.kamal1489@gmail.com>
2024-11-10 17:53:05 -0500

    removed NavBar from App and PLP components

--- ecommerce-app/src/App.tsx
+++ ecommerce-app/src/App.tsx
@@ -2,13 +2,11 @@ import Footer from "./components/Footer/Footer";
 import HeroSection from "./components/HeroSection/HeroSection";
 import Category from "./components/Section/Category/Category";
 import NewProducts from "./components/Section/NewProducts";
-import NavBar from "./components/NavBar/NavBar";
 import content from "./data/content.json";
 
 function App() {
   return (
     <>
-      <NavBar />
       <HeroSection />
       <NewProducts />
       {content?.category &&
--- ecommerce-app/src/pages/ProductListPage/ProductListPage.tsx
+++ ecommerce-app/src/pages/ProductListPage/ProductListPage.tsx
@@ -1,17 +1,18 @@
 import React from "react";
-import NavBar from "../../components/NavBar/NavBar";
 import FilterIcon from "../../components/Common/FilterIcon";
 
 const ProductListPage = () => {
   return (
     <div>
-      <NavBar />
       <div className="flex">
         <div className="w-[30%] p-[20px] border rounded-lg m-[20px]">
           {/* filters */}
           <div className="flex justify-between">
-            <p className="text-lg">Filters</p>
+            <p className="text-[16px] text-gray-600">Filter</p>
             <FilterIcon />
+          </div>
+          <div>
+            <p className="text-[16px] text-black mt-5">Categories</p>
           </div>
         </div>
         <div className="p-[40px]">
