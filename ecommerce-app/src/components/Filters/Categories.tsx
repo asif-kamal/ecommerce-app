998ad8086915863bf474731a674b98872afb2fa4
asif-kamal <asif.kamal1489@gmail.com>
2024-11-15 13:20:00 -0500

    added htmlFor for, code and displayed name of type inside label tag

--- ecommerce-app/src/components/Filters/Categories.tsx
+++ ecommerce-app/src/components/Filters/Categories.tsx
@@ -2,11 +2,13 @@ import React from "react";
 
 interface CategoryType {
   code: string;
+  name: string;
 }
 
 interface CategoriesProps {
   types: CategoryType[];
 }
+
 
 const Categories: React.FC<CategoriesProps> = ({ types }) => {
   return (
@@ -19,7 +21,7 @@ const Categories: React.FC<CategoriesProps> = ({ types }) => {
               name={type?.code}
               className="border rounded-lg w-4 h-4 accent-black text-black"
             />
-            <label htmlFor={type?.code}></label>
+            <label htmlFor={type?.code}>{type?.name}</label>
           </div>
         );
       })}
