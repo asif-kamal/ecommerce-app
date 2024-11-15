8b6c7a135bb26add6e4e2dbf16b3643e6736c525
asif-kamal <asif.kamal1489@gmail.com>
2024-11-14 21:24:18 -0500

    built a checkbox for each CODE

--- ecommerce-app/src/components/Filters/Categories.tsx
+++ ecommerce-app/src/components/Filters/Categories.tsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import React from "react";
 
-const Categories = () => {
-  return (
-    <div>
-        
-    </div>
-  )
+interface CategoryType {
+  code: string;
 }
 
-export default Categories
+interface CategoriesProps {
+  types: CategoryType[];
+}
+
+const Categories: React.FC<CategoriesProps> = ({ types }) => {
+  return (
+    <div>
+      {types?.map((type) => {
+        return (
+          <div className="flex" key={type.code}>
+            <input
+              type="checkbox"
+              name={type?.code}
+              className="border rounded-lg w-4 h-4 accent-black text-black"
+            />
+            <label htmlFor={type?.code}></label>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Categories;
