0fb6952a5cb5423b586890d6e0c3dd6f5c586d32
asif-kamal <asif.kamal1489@gmail.com>
2024-11-21 14:15:24 -0500

    converted colorSelector to TS format and displayed colors filter on PLP

--- ecommerce-app/src/components/Filters/ColorsFilter.tsx
+++ ecommerce-app/src/components/Filters/ColorsFilter.tsx
@@ -1,24 +1,56 @@
-import React from 'react'
+import React from "react";
 
-const colorSelector = {
-    "Purple":"#8434E1",
-    "Black":"#252525",
-    "White":"#FFFFFF",
-    "Gray": "#808080",
-    "Blue": "#0000FF",
-    "Red": "#FF0000",
-    "Orange": "#FFA500",
-    "Navy": "#000080",
-    "Grey": "#808080",
-    "Yellow": "#FFFF00",
-    "Pink": "#FFC0CB",
-    "Green": "#008000"
+type ColorKey =
+  | "Purple"
+  | "Black"
+  | "White"
+  | "Gray"
+  | "Blue"
+  | "Red"
+  | "Orange"
+  | "Navy"
+  | "Grey"
+  | "Yellow"
+  | "Pink"
+  | "Green";
+const colorSelector: { [key in ColorKey]: string } = {
+  Purple: "#8434E1",
+  Black: "#252525",
+  White: "#FFFFFF",
+  Gray: "#808080",
+  Blue: "#0000FF",
+  Red: "#FF0000",
+  Orange: "#FFA500",
+  Navy: "#000080",
+  Grey: "#808080",
+  Yellow: "#FFFF00",
+  Pink: "#FFC0CB",
+  Green: "#008000",
+};
+
+interface ColorsFilterProps {
+  colors: string[];
 }
 
-const ColorsFilter = ({colors}) => {
+const ColorsFilter: React.FC<ColorsFilterProps> = ({ colors }) => {
   return (
-    <div>ColorsFilter</div>
-  )
-}
+    <div className="flex flex-col mb-4">
+      <p className="text-[16px] text-black mt-5">Colors</p>
+      <div className="flex flex-wrap p-4">
+        {(colors as ColorKey[])?.map((item: ColorKey) => {
+          return (
+            <div key={item} className="flex flex-col gap-1">
+              <div
+                className="w-8 h-8 border rounded-xl mr-4 mb-4"
+                style={{ background: `${colorSelector[item]}` }}
+              ></div>
+              <p>{item}</p>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
 
-export default ColorsFilter
+export default ColorsFilter;
