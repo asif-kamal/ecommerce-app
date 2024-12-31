cebd59e65f01bc9b4dcacbe8f470d94e242e3c37
asif-kamal <asif.kamal1489@gmail.com>
2024-12-31 11:23:53 -0600

    Working with applied size and styling on click

--- ecommerce-app/src/components/Filters/SizeFilter.tsx
+++ ecommerce-app/src/components/Filters/SizeFilter.tsx
@@ -1,16 +1,42 @@
+import { useState, useCallback } from "react";
+
 interface SizeFilterProps {
   sizes: string[];
 }
 
 const SizeFilter = ({ sizes }: SizeFilterProps) => {
+  const [appliedSize, setAppliedSize] = useState<string[]>([]);
+  interface OnClickDiv {
+    (item: string): void;
+  }
+
+  const onClickDiv: OnClickDiv = useCallback(
+    (item: string) => {
+      if (appliedSize.indexOf(item) > -1) {
+        setAppliedSize(appliedSize.filter((size: string) => size !== item));
+      } else {
+        setAppliedSize([...appliedSize, item]);
+      }
+    },
+    [appliedSize, setAppliedSize]
+  );
+
   return (
     <div className="flex flex-col mb-4">
       <p className="text-[16px] text-black mt-5">Sizes</p>
       <div className="flex flex-wrap p-4">
-        {sizes?.map((item : string) => {
+        {sizes?.map((item: string) => {
           return (
             <div key={item} className="flex flex-col mr-2">
-              <div className="w-[50px] h-8 border text-center mb-4 rounded-lg mr-4 cursor-pointer hover:scale-110 bg-white border-gray-500 text-gray-500">
+              <div
+                className="w-[50px] h-8 border text-center mb-4 rounded-lg mr-4 cursor-pointer hover:scale-110 bg-white border-gray-500 text-gray-500"
+                style={
+                  appliedSize.includes(item)
+                    ? { background: "black", color: "white" }
+                    : {}
+                }
+                onClick={() => onClickDiv(item)}
+              >
                 {item}
               </div>
             </div>
